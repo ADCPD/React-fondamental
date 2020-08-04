e09c7c8efd52fd2024ae6cfa44718ddbc08c6219
adhaouadi <dhaouadi.amir@gmail.com>
2020-08-04 13:07:10 +0200

    Methods as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import EventBind from './components/EventHandler/EventBind';
+import ParentComponent from './components/MethodAsProps/ParentComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <EventBind /> 
+         <ParentComponent /> 
       </div>
     );
  }
